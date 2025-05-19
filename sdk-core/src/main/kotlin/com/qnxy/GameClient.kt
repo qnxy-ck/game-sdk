53607cd2ec89711ac44213d3e017b92e72f08f4d
qnxy <qnxy1997@gmail.com>
2025-05-20 01:43:34 +0800

    impl categoryInfo

--- sdk-core/src/main/kotlin/com/qnxy/GameClient.kt
+++ sdk-core/src/main/kotlin/com/qnxy/GameClient.kt
@@ -1,11 +1,13 @@
 package com.qnxy
 
+import com.qnxy.data.GameCategoryListResponse
 import com.qnxy.data.GameLaunchUrlResp
 import com.qnxy.data.LaunchUrlReq
 import com.qnxy.data.ReturnValue
 import com.qnxy.internal.InternalHttpClient
 import io.ktor.util.reflect.*
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.builtins.serializer
 import java.util.concurrent.CompletableFuture
 
 
@@ -39,6 +41,22 @@ public class GameClient(
         return runBlocking { launchUrl(launchUrlReq) }
     }
 
+    public suspend fun categoryInfo(): ReturnValue<GameCategoryListResponse?> {
+        return internalHttpClient.post(
+            "/game/getCategory",
+            Unit,
+            Unit.serializer(),
+            typeInfo<ReturnValue<GameCategoryListResponse?>>()
+        )
+    }
+
+    public fun categoryInfoFuture(): CompletableFuture<ReturnValue<GameCategoryListResponse?>> {
+        return internalHttpClient.future { categoryInfo() }
+    }
+
+    public fun categoryInfoBlocking(): ReturnValue<GameCategoryListResponse?> {
+        return runBlocking { categoryInfo() }
+    }
 
     public enum class EndpointEnvironment(public val url: String) {
         TEST("https://pre-api.agsoftgames.com/game/v3"),
--- sdk-core/src/main/kotlin/com/qnxy/data/RequestDatas.kt
+++ sdk-core/src/main/kotlin/com/qnxy/data/RequestDatas.kt
@@ -25,6 +25,14 @@ public data class LaunchUrlReq(
     val returnUrl: String? = null
 ) : RequestData
 
+@Serializable
+public data class SetPlayerRtpReq(
+    val currencyCode: String,
+    val rtp: Int,
+    val playerUniqueId: String,
+    val subMerchantCode: String? = null
+)
+
 
 @Serializable
 public data class VerifySessionReq(
@@ -38,3 +46,4 @@ public data class BalanceReq(
     val currencyCode: String
 
 )
+
